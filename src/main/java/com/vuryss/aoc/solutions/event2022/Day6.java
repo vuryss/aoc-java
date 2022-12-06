d0038f89e331915f3c6e44198f3c46c064ddbbb8
Georgi Georgiev <georgi@designtechnologies.com>
2022-12-06 09:55:20 +0200

    Day 6

---
+++ src/main/java/com/vuryss/aoc/solutions/event2022/Day6.java
@@ -0,0 +1,68 @@
+package com.vuryss.aoc.solutions.event2022;
+
+import com.vuryss.aoc.DayInterface;
+
+import java.util.*;
+
+public class Day6 implements DayInterface {
+    @Override
+    public Map<String, String> part1Tests() {
+        return Map.of(
+            "mjqjpqmgbljsphdztnvjfqwrcgsmlb",
+            "7",
+            "bvwbjplbgvbhsrlpgdmjqwftvncz",
+            "5",
+            "nppdvjthqldpwncqszvftbrmjlhg",
+            "6",
+            "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg",
+            "10",
+            "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw",
+            "11"
+        );
+    }
+
+    @Override
+    public Map<String, String> part2Tests() {
+        return Map.of(
+            "mjqjpqmgbljsphdztnvjfqwrcgsmlb",
+            "19",
+            "bvwbjplbgvbhsrlpgdmjqwftvncz",
+            "23",
+            "nppdvjthqldpwncqszvftbrmjlhg",
+            "23",
+            "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg",
+            "29",
+            "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw",
+            "26"
+        );
+    }
+
+    @Override
+    public String part1Solution(String input) {
+        return getFirstBatchOfUniqueCharactersLastPosition(input, 4);
+    }
+
+    @Override
+    public String part2Solution(String input) {
+        return getFirstBatchOfUniqueCharactersLastPosition(input, 14);
+    }
+
+    private String getFirstBatchOfUniqueCharactersLastPosition(String input, int uniqueCharactersCount) {
+        var characters = input.toCharArray();
+        var index = 0;
+
+        while (true) {
+            var set = new HashSet<Character>();
+
+            for (var i = 0; i < uniqueCharactersCount; i++) {
+                set.add(characters[index + i]);
+            }
+
+            if (set.size() == uniqueCharactersCount) {
+                return String.valueOf(index + uniqueCharactersCount);
+            }
+
+            index++;
+        }
+    }
+}
