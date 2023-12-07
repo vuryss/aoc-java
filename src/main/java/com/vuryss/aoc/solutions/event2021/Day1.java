86a16b09828ccadc436de8f44a78b92802e9c9a4
Georgi Georgiev <vuryss@gmail.com>
2023-12-07 19:11:02 +0200

    Year 2021, Day 1

---
+++ src/main/java/com/vuryss/aoc/solutions/event2021/Day1.java
@@ -0,0 +1,65 @@
+package com.vuryss.aoc.solutions.event2021;
+
+import com.vuryss.aoc.solutions.DayInterface;
+import com.vuryss.aoc.util.ListUtil;
+import java.util.Map;
+import java.util.stream.IntStream;
+
+@SuppressWarnings("unused")
+public class Day1 implements DayInterface {
+    @Override
+    public Map<String, String> part1Tests() {
+        return Map.of(
+            """
+            199
+            200
+            208
+            210
+            200
+            207
+            240
+            269
+            260
+            263
+            """,
+            "7"
+        );
+    }
+
+    @Override
+    public Map<String, String> part2Tests() {
+        return Map.of(
+            """
+            199
+            200
+            208
+            210
+            200
+            207
+            240
+            269
+            260
+            263
+            """,
+            "5"
+        );
+    }
+
+    @Override
+    public String part1Solution(String input) {
+        var n = ListUtil.extractUnsignedIntegers(input);
+        var count = IntStream.range(1, n.size()).filter(i -> n.get(i) > n.get(i - 1)).count();
+
+        return String.valueOf(count);
+    }
+
+    @Override
+    public String part2Solution(String input) {
+        var n = ListUtil.extractUnsignedIntegers(input);
+        var count = IntStream.range(3, n.size())
+            .filter(i -> n.get(i) + n.get(i - 1) + n.get(i - 2) > n.get(i - 1) + n.get(i - 2) + n.get(i - 3))
+            .count();
+
+        return String.valueOf(count);
+    }
+}
