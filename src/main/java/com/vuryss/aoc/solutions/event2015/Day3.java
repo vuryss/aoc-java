5f26d19750b027d62f89ea3bec7408a1514099e7
Georgi Georgiev <vuryss@gmail.com>
2023-12-08 14:24:57 +0200

    Year 2015

---
+++ src/main/java/com/vuryss/aoc/solutions/event2015/Day3.java
@@ -0,0 +1,76 @@
+package com.vuryss.aoc.solutions.event2015;
+
+import com.vuryss.aoc.solutions.DayInterface;
+import com.vuryss.aoc.util.Point;
+import com.vuryss.aoc.util.Reference;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@SuppressWarnings("unused")
+public class Day3 implements DayInterface {
+    @Override
+    public Map<String, String> part1Tests() {
+        return Map.of(
+            ">", "2",
+            "^>v<", "4",
+            "^v^v^v^v^v", "2"
+        );
+    }
+
+    @Override
+    public Map<String, String> part2Tests() {
+        return Map.of(
+            "^v", "3",
+            "^>v<", "3",
+            "^v^v^v^v^v", "11"
+        );
+    }
+
+    @Override
+    public String part1Solution(String input) {
+        var chars = input.trim().toCharArray();
+        var map = new HashMap<Point, Integer>();
+        var position = new Point(0, 0);
+        map.put(position, 1);
+
+        for (var c: chars) {
+            switch (c) {
+                case '^' -> position = position.add(new Point(0, 1));
+                case 'v' -> position = position.add(new Point(0, -1));
+                case '>' -> position = position.add(new Point(1, 0));
+                case '<' -> position = position.add(new Point(-1, 0));
+            }
+
+            map.put(position, map.getOrDefault(position, 0) + 1);
+        }
+
+        return String.valueOf(map.size());
+    }
+
+    @Override
+    public String part2Solution(String input) {
+        var chars = input.trim().toCharArray();
+        var map = new HashMap<Point, Integer>();
+        var santaPosition = new Reference<>(new Point(0, 0));
+        var roboSantaPosition = new Reference<>(new Point(0, 0));
+        map.put(santaPosition.value, 2);
+        boolean isSanta = true;
+
+        for (var c: chars) {
+            var position = isSanta ? santaPosition : roboSantaPosition;
+            isSanta = !isSanta;
+
+            switch (c) {
+                case '^' -> position.value = position.value.add(new Point(0, 1));
+                case 'v' -> position.value = position.value.add(new Point(0, -1));
+                case '>' -> position.value = position.value.add(new Point(1, 0));
+                case '<' -> position.value = position.value.add(new Point(-1, 0));
+            }
+
+            map.put(position.value, map.getOrDefault(position.value, 0) + 1);
+        }
+
+        return String.valueOf(map.size());
+    }
+}
---
+++ src/main/java/com/vuryss/aoc/util/Reference.java
@@ -0,0 +1,9 @@
+package com.vuryss.aoc.util;
+
+public class Reference<T> {
+    public T value;
+
+    public Reference(T value) {
+        this.value = value;
+    }
+}
