a41e743838f3b47fa160bf61b265c112d0313c00
Georgi Georgiev <vuryss@gmail.com>
2023-12-09 10:21:35 +0200

    Year 2015

---
+++ src/main/java/com/vuryss/aoc/solutions/event2015/Day8.java
@@ -0,0 +1,70 @@
+package com.vuryss.aoc.solutions.event2015;
+
+import com.vuryss.aoc.solutions.DayInterface;
+import org.apache.commons.text.StringEscapeUtils;
+
+import java.util.Map;
+
+@SuppressWarnings("unused")
+public class Day8 implements DayInterface {
+    @Override
+    public Map<String, String> part1Tests() {
+        return Map.of(
+            """
+            ""
+            "abc"
+            "aaa\\"aaa"
+            "\\x27"
+            """,
+            "12"
+        );
+    }
+
+    @Override
+    public Map<String, String> part2Tests() {
+        return Map.of(
+            """
+            ""
+            "abc"
+            "aaa\\"aaa"
+            "\\x27"
+            """,
+            "19"
+        );
+    }
+
+    @Override
+    public String part1Solution(String input) {
+        var lines = input.trim().split("\n");
+        var sum = 0;
+
+        for (var line: lines) {
+            var trimmedLine = line
+                .substring(1, line.length() - 1)
+                .replaceAll("\\\\\\\\", "\\\\")
+                .replaceAll("\\\\x[0-9a-f]{2}", "x")
+                .replaceAll("\\\\\"", "\"");
+
+            sum += line.length() - trimmedLine.length();
+        }
+
+        return String.valueOf(sum);
+    }
+
+    @Override
+    public String part2Solution(String input) {
+        var lines = input.trim().split("\n");
+        var sum = 0;
+
+        for (var line: lines) {
+            var escapedLine = line
+                .replaceAll("\\\\", "\\\\\\\\")
+                .replaceAll("\"", "\\\\\"");
+            escapedLine = "\"" + escapedLine + "\"";
+
+            sum += escapedLine.length() - line.length();
+        }
+
+        return String.valueOf(sum);
+    }
+}
