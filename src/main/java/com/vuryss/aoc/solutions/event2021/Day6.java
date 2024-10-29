f1a74b21fc5965fe659da874b22063a3b666854c
Georgi Georgiev <vuryss@gmail.com>
2024-10-29 17:13:59 +0200

    Year 2021, Day 6

---
+++ src/main/java/com/vuryss/aoc/solutions/event2021/Day6.java
@@ -0,0 +1,69 @@
+package com.vuryss.aoc.solutions.event2021;
+
+import com.vuryss.aoc.solutions.DayInterface;
+import com.vuryss.aoc.util.StringUtil;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@SuppressWarnings("unused")
+public class Day6 implements DayInterface {
+    @Override
+    public Map<String, String> part1Tests() {
+        return Map.of(
+            """
+            3,4,3,1,2
+            """,
+            "5934"
+        );
+    }
+
+    @Override
+    public Map<String, String> part2Tests() {
+        return Map.of(
+            """
+            3,4,3,1,2
+            """,
+            "26984457539"
+        );
+    }
+
+    @Override
+    public String part1Solution(String input) {
+        return String.valueOf(reproduceLanternfish(input, 80));
+    }
+
+    @Override
+    public String part2Solution(String input) {
+        return String.valueOf(reproduceLanternfish(input, 256));
+    }
+
+    private long reproduceLanternfish(String input, int days) {
+        var lanternfish = StringUtil.ints(input);
+        var map = new HashMap<Integer, Long>();
+
+        for (var l: lanternfish) {
+            map.put(l, map.getOrDefault(l, 0L) + 1);
+        }
+
+        for (var i = 0; i < days; i++) {
+            var newMap = new HashMap<Integer, Long>();
+
+            for (var entry: map.entrySet()) {
+                var timeToReproduce = entry.getKey();
+                var numberOfLanternfish = entry.getValue();
+
+                if (timeToReproduce == 0) {
+                    newMap.put(6, newMap.getOrDefault(6, 0L) + numberOfLanternfish);
+                    newMap.put(8, newMap.getOrDefault(8, 0L) + numberOfLanternfish);
+                } else {
+                    newMap.put(timeToReproduce - 1, newMap.getOrDefault(timeToReproduce - 1, 0L) + numberOfLanternfish);
+                }
+            }
+
+            map = newMap;
+        }
+
+        return map.values().stream().mapToLong(Long::longValue).sum();
+    }
+}
