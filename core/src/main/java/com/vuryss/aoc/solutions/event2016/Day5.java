7911778ae06aaebadcebef908e7d21e5b82f1ef0
Georgi Georgiev <vuryss@gmail.com>
2025-08-08 14:07:25 +0300

    feat: Event 2016 Day 5 + commons codec

---
+++ core/src/main/java/com/vuryss/aoc/solutions/event2016/Day5.java
@@ -0,0 +1,64 @@
+package com.vuryss.aoc.solutions.event2016;
+
+import com.vuryss.aoc.solutions.SolutionInterface;
+import org.apache.commons.codec.digest.DigestUtils;
+
+import java.util.HashSet;
+import java.util.Map;
+
+@SuppressWarnings("unused")
+public class Day5 implements SolutionInterface {
+    @Override
+    public Map<String, String> part1Tests() {
+        return Map.of(
+            "abc", "18f47a30"
+        );
+    }
+
+    @Override
+    public Map<String, String> part2Tests() {
+        return Map.of(
+            "abc", "05ace8e3"
+        );
+    }
+
+    @Override
+    public String part1Solution(String input) {
+        input = input.trim();
+        var index = 0;
+        var doorId = new StringBuilder();
+
+        while (doorId.length() < 8) {
+            var hash = DigestUtils.md5Hex(input + index++);
+
+            if (hash.startsWith("00000")) {
+                doorId.append(hash.charAt(5));
+            }
+        }
+
+        return doorId.toString();
+    }
+
+    @Override
+    public String part2Solution(String input) {
+        input = input.trim();
+        var index = 0;
+        var chars = new char[8];
+        var usedPositions = new HashSet<Integer>();
+
+        while (usedPositions.size() < 8) {
+            var hash = DigestUtils.md5Hex(input + index++);
+
+            if (hash.startsWith("00000")) {
+                var position = Character.getNumericValue(hash.charAt(5));
+
+                if (position >= 0 && position < 8 && !usedPositions.contains(position)) {
+                    chars[position] = hash.charAt(6);
+                    usedPositions.add(position);
+                }
+            }
+        }
+
+        return new String(chars);
+    }
+}
