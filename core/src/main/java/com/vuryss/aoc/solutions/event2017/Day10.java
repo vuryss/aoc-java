dd46520e31ad1d7ca4d725c1d86cc2ab6f61c9ee
Georgi Georgiev <vuryss@gmail.com>
2025-11-20 21:07:35 +0200

    feat: Event 2017 day 10

---
+++ core/src/main/java/com/vuryss/aoc/solutions/event2017/Day10.java
@@ -0,0 +1,98 @@
+package com.vuryss.aoc.solutions.event2017;
+
+import com.vuryss.aoc.solutions.SolutionInterface;
+import com.vuryss.aoc.util.StringUtil;
+
+import java.lang.Override;
+import java.lang.String;
+import java.lang.SuppressWarnings;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+@SuppressWarnings("unused")
+public class Day10 implements SolutionInterface {
+    @Override
+    public Map<String, String> part1Tests() {
+        return Map.of(
+            "3, 4, 1, 5", "12"
+        );
+    }
+
+    @Override
+    public Map<String, String> part2Tests() {
+        return Map.of();
+    }
+
+    @Override
+    public String part1Solution(String input, boolean isTest) {
+        var lengths = StringUtil.ints(input);
+        var list = new ArrayList<>(
+            isTest ? List.of(0, 1, 2, 3, 4) : IntStream.range(0, 256).boxed().collect(Collectors.toList())
+        );
+        var position = 0;
+        var skipSize = 0;
+
+        for (var length: lengths) {
+            Collections.reverse(list.subList(0, length));
+            Collections.rotate(list, -length - skipSize);
+            position += length + skipSize;
+            skipSize++;
+        }
+
+        Collections.rotate(list, position % list.size());
+
+        return String.valueOf(list.get(0) * list.get(1));
+    }
+
+    @Override
+    public String part2Solution(String input, boolean isTest) {
+        var lengths = new ArrayList<Integer>();
+
+        for (var ch: input.trim().toCharArray()) {
+            lengths.add((int) ch);
+        }
+
+        lengths.addAll(List.of(17, 31, 73, 47, 23));
+
+        var list = new ArrayList<>(
+            isTest ? List.of(0, 1, 2, 3, 4) : IntStream.range(0, 256).boxed().collect(Collectors.toList())
+        );
+        var position = 0;
+        var skipSize = 0;
+
+        for (var round = 0; round < 64; round++) {
+            for (var length: lengths) {
+                Collections.reverse(list.subList(0, length));
+                Collections.rotate(list, -length - skipSize);
+                position += length + skipSize;
+                skipSize++;
+            }
+        }
+
+        Collections.rotate(list, position % list.size());
+
+        var denseHash = new ArrayList<Integer>();
+
+        for (var i = 0; i < 16; i++) {
+            var xor = 0;
+
+            for (var j = 0; j < 16; j++) {
+                xor ^= list.get(i * 16 + j);
+            }
+
+            denseHash.add(xor);
+        }
+
+        var hex = new StringBuilder();
+
+        for (var i = 0; i < 16; i++) {
+            hex.append(String.format("%02x", denseHash.get(i)));
+        }
+
+        return hex.toString();
+    }
+}
