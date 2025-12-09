d1c502f85b39343c40479da5abb62612597807e7
Georgi Georgiev <vuryss@gmail.com>
2025-12-09 11:29:09 +0200

    refactor: Event 2025 day 9

--- core/src/main/java/com/vuryss/aoc/solutions/event2025/Day9.java
+++ core/src/main/java/com/vuryss/aoc/solutions/event2025/Day9.java
@@ -1,7 +1,6 @@
 package com.vuryss.aoc.solutions.event2025;
 
 import com.vuryss.aoc.solutions.SolutionInterface;
-import com.vuryss.aoc.util.Combinatorics;
 import com.vuryss.aoc.util.Point;
 import com.vuryss.aoc.util.StringUtil;
 
@@ -9,7 +8,6 @@ import java.lang.Override;
 import java.lang.String;
 import java.lang.SuppressWarnings;
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.Map;
 
 @SuppressWarnings("unused")
@@ -50,26 +48,28 @@ public class Day9 implements SolutionInterface {
 
     @Override
     public String part1Solution(String input, boolean isTest) {
-        var lines = input.trim().split("\n");
-        var points = new ArrayList<Point>();
+        var inputLines = input.trim().split("\n");
+        var pointsList = new ArrayList<Point>();
         long maxArea = 0;
 
-        for (var line: lines) {
+        for (var line: inputLines) {
             var coordinates = StringUtil.ints(line);
-            points.add(new Point(coordinates.get(0), coordinates.get(1)));
+            pointsList.add(new Point(coordinates.get(0), coordinates.get(1)));
         }
 
-        var combinations = Combinatorics.combinations(points, 2);
+        var points = pointsList.toArray(new Point[0]);
 
-        for (var combination: combinations) {
-            var point1 = combination.get(0);
-            var point2 = combination.get(1);
-            long width = Math.abs(point1.x - point2.x) + 1;
-            long height = Math.abs(point1.y - point2.y) + 1;
-            long area = width * height;
+        for (var i = 0; i < points.length - 1; i++) {
+            for (var j = i + 1; j < points.length; j++) {
+                long minX = Math.min(points[i].x, points[j].x);
+                long maxX = Math.max(points[i].x, points[j].x);
+                long minY = Math.min(points[i].y, points[j].y);
+                long maxY = Math.max(points[i].y, points[j].y);
+                long area = (maxX - minX + 1) * (maxY - minY + 1);
 
-            if (area > maxArea) {
-                maxArea = area;
+                if (area > maxArea) {
+                    maxArea = area;
+                }
             }
         }
 
@@ -78,78 +78,49 @@ public class Day9 implements SolutionInterface {
 
     @Override
     public String part2Solution(String input, boolean isTest) {
-        var lines = input.trim().split("\n");
-        var points = new ArrayList<Point>();
+        var inputLines = input.trim().split("\n");
+        var pointsList = new ArrayList<Point>();
         long maxArea = 0;
 
-        for (var line: lines) {
+        for (var line: inputLines) {
             var coordinates = StringUtil.ints(line);
-            points.add(new Point(coordinates.get(0), coordinates.get(1)));
+            pointsList.add(new Point(coordinates.get(0), coordinates.get(1)));
         }
 
-        var allLines = new ArrayList<Line>();
-        var allColumns = new ArrayList<Column>();
+        var points = pointsList.toArray(new Point[0]);
+        var lines = new Point[points.length][2];
 
-        points.sort(Comparator.<Point>comparingInt(point -> point.x).thenComparingInt(point -> point.y));
-
-        for (var i = 0; i < points.size() - 1; i += 2) {
-            allColumns.add(new Column(points.get(i).x, points.get(i).y, points.get(i + 1).y));
+        for (var i = 0; i < points.length; i++) {
+            lines[i][0] = points[i];
+            lines[i][1] = points[(i+1)%points.length];
         }
 
-        points.sort(Comparator.<Point>comparingInt(point -> point.y).thenComparingInt(point -> point.x));
+        for (var i = 0; i < points.length - 1; i++) {
+            nextCombination:
+            for (var j = i + 1; j < points.length; j++) {
+                long minX = Math.min(points[i].x, points[j].x);
+                long maxX = Math.max(points[i].x, points[j].x);
+                long minY = Math.min(points[i].y, points[j].y);
+                long maxY = Math.max(points[i].y, points[j].y);
+                long area = (maxX - minX + 1) * (maxY - minY + 1);
 
-        for (var i = 0; i < points.size() - 1; i += 2) {
-            allLines.add(new Line(points.get(i).y, points.get(i).x, points.get(i + 1).x));
-        }
+                if (area > maxArea) {
+                    for (var line: lines) {
+                        long lineMinX = Math.min(line[0].x, line[1].x);
+                        long lineMaxX = Math.max(line[0].x, line[1].x);
+                        long lineMinY = Math.min(line[0].y, line[1].y);
+                        long lineMaxY = Math.max(line[0].y, line[1].y);
 
-        var combinations = Combinatorics.combinations(points, 2);
+                        if (lineMinX < maxX && lineMaxX > minX && lineMinY < maxY && lineMaxY > minY) {
+                            continue nextCombination;
+                        }
+                    }
 
-        nextCombination:
-        for (var combination: combinations) {
-            var point1 = combination.get(0);
-            var point2 = combination.get(1);
-
-            long minX = Math.min(point1.x, point2.x);
-            long maxX = Math.max(point1.x, point2.x);
-            long minY = Math.min(point1.y, point2.y);
-            long maxY = Math.max(point1.y, point2.y);
-
-            for (var line: allLines) {
-                if (line.y <= minY || line.y >= maxY) {
-                    continue;
+                    maxArea = area;
                 }
-
-                if (
-                    (minX < line.from && maxX > line.from || minX < line.to && maxX > line.to) // Line Point inside the rectangle
-                        || (minX >= line.from && maxX <= line.to) // Rectangle inside the line
-                ) {
-                    continue nextCombination;
-                }
-            }
-
-            for (var column: allColumns) {
-                if (column.x <= minX || column.x >= maxX) {
-                    continue;
-                }
-
-                if (
-                    (minY < column.from && maxY > column.from || minY < column.to && maxY > column.to) // Line Point inside the rectangle
-                        || (minY >= column.from && maxY <= column.to) // Rectangle inside the line
-                ) {
-                    continue nextCombination;
-                }
-            }
-
-            long area = (maxX - minX + 1) * (maxY - minY + 1);
-
-            if (area > maxArea) {
-                maxArea = area;
             }
         }
 
         return String.valueOf(maxArea);
     }
-
-    private record Line(int y, int from, int to) {}
-    private record Column(int x, int from, int to) {}
 }
